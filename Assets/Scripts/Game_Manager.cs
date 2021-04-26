593e14af4a1dc92f498d27d117dfd7660eace7d2
Miquel Nadal <miquel.nadal@enti.cat>
2021-04-26 13:44:08 +0200

    Scripts Importats

---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Game_Manager : MonoBehaviour
+{
+    public static Game_Manager Instance { get; private set; }
+    public Scene currentScene;
+   
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        } else
+        {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+        }
+        
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/Movement_Script.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movement_Script : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    private float speedx;
+    public Rigidbody2D rb;
+    private int counter = 0;
+    public float maxX = 5;
+    public AudioSource footstepsGrass;
+    public SpriteRenderer sr;
+
+    
+    private Animator animator;
+    private int walkParamID;
+    private int jumpParamID;
+
+   
+    float moveVelocity;
+    public bool grounded = true;
+
+    public void Start()
+    {
+        footstepsGrass = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        walkParamID = Animator.StringToHash("Walk");
+        jumpParamID = Animator.StringToHash("Jump");
+       
+    }
+
+    void Update()
+    {
+      
+         bool isJumping = false;
+        //Jumping 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           isJumping = true;
+            if (!grounded)
+            {
+                if (counter < 1)
+                {
+                    rb.AddForce(transform.up * jump, ForceMode2D.Impulse);
+                    counter++;
+                }
+            }
+            else
+            {
+                rb.AddForce(transform.up * jump, ForceMode2D.Impulse);
+                counter = 0;
+            }
+        }
+
+        //Animator
+        if (isJumping)
+        {Debug.Log("salto");
+            isJumping = animator.GetBool(jumpParamID);
+            animator.SetBool(jumpParamID, true);
+        }
+        else {
+            isJumping = animator.GetBool(jumpParamID);
+            animator.SetBool(jumpParamID, false);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float delta = Time.fixedDeltaTime;
+
+        bool isWalking = false;
+
+
+        //Left Right Movement 
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            isWalking = true;
+            if (rb.velocity.x > -maxX)
+            {
+                rb.AddForce((Vector2.left * speed), ForceMode2D.Force);
+                sr.flipX = true;
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+            isWalking = true;
+            if (rb.velocity.x < maxX)
+            {
+                rb.AddForce((Vector2.right * speed) , ForceMode2D.Force);
+                sr.flipX = false;
+            }
+            
+        } 
+
+        //Animator
+        if (isWalking)
+        {
+            isWalking = animator.GetBool(walkParamID);
+            animator.SetBool(walkParamID, true);
+        }
+        else
+        {
+            isWalking = animator.GetBool(walkParamID);
+            animator.SetBool(walkParamID, false);
+        }
+
+    }
+    //Check if Grounded 
+    void OnCollisionStay2D()
+    {
+        grounded = true;
+        
+    }
+    void OnCollisionExit2D()
+    {
+        grounded = false;
+    }
+
+   
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+         if (collision.gameObject.tag == "grass") {
+            footstepsGrass.Play();
+         }
+
+        if (collision.gameObject.tag == "Floppa")
+        {
+            SceneManager.LoadScene("EasterEgg");
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+   {
+        if (collision.gameObject.tag == "car")
+        {
+            //Game_Manager.Instance.LoadNextScene();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("NEXT");
+        }
+
+        if (collision.gameObject.tag == "Finale") 
+        {
+            SceneManager.LoadScene("Final");
+        }
+
+        
+    }
+    
+}

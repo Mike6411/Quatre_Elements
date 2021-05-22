ff44c50f9d310b42774b6286e0788c0fe8b8a76d
Miquel Nadal <miquel.nadal@enti.cat>
2021-05-22 15:22:43 +0200

    Mouse Aim y Colisión de proyectiles

---
+++ Assets/Scripts/Blaster_Aim.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaster_Aim : MonoBehaviour
+{
+    private int maxAngle = 90;
+    public GameObject jugador;
+    public GameObject rocket;
+    public float rocketSpeed;
+
+    public float shotDelay = 0.5f;
+    public float elapsedTime = 0;
+
+    void Start()
+    {
+        elapsedTime = 1f;
+    }
+
+    void Update()
+    {
+        //Rotation Call
+        RotationWeapon(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        //Shot delay stuff
+        elapsedTime += Time.deltaTime;
+
+        //Weapon Use
+        if (Input.GetKeyDown(KeyCode.Mouse0) && elapsedTime >= shotDelay)
+        {
+        elapsedTime = 0;
+        GameObject newRocket = Instantiate(rocket, transform.position + transform.right, transform.rotation);
+        newRocket.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * rocketSpeed);
+        }
+    }
+
+    public void RotationWeapon(Vector3 mousePos)
+    {
+        Vector2 dir = mousePos - transform.position;
+        float rotationZ = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(0, 0, rotationZ);
+    }
+
+}
